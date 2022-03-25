a3c566785b7651ccdee679a862f83a5d82678223
Jojo0804-hub <60533961+Jojo0804-hub@users.noreply.github.com>
2022-03-25 16:54:36 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var marks = [30,40,45,35];
+
+function marksAverage(){
+var sum=0
+for(var i=0;i<marks.length;i++){
+  sum=sum+marks[i]
+}
+var avg=sum/marks.length
+console.log(avg)
+
+
+
+
+
+
+
+}
+function setup() {
+  createCanvas(400, 400);
+marksAverage();
+}
+
+function draw() {
+  background(150);
+}
